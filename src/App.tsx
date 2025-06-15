a72fd144d60b4339da344a0243addfa3d7b7316e
dineshdkulkarni <dineshdkulkarni@gmail.com>
2025-06-15 18:14:51 +0530

    Update package json

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my first web site on AWS. 
+          This is my first web site on Azure.... 
         </p>
         <a
           className="App-link"
